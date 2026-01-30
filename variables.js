83439a314bf522ec6b97f9c2f9ab09a76c289c36
Javier Martinez <javier.martinezperches01@gmail.com>
2026-01-29 19:27:56 -0600

    feat: variables y operadores

---
+++ variables.js
@@ -0,0 +1,48 @@
+let mascota = 'perro';
+let mascota1 = 'gato';
+
+const numero = 23;
+
+let num1 = 3;
+let num2 = 5;
+
+let suma = num1 + num2;
+let resta =num2 - num1;
+let modulo =num2 % num1;
+
+console.log(suma);
+console.log(resta);
+console.log("Modulo de 3 % 5: ", modulo);
+
+console.log(num1*num2);
+console.log(num2/num1);
+
+//Comparacion entre dos variables, simples
+console.log(num1 == num2);
+
+console.log(10 == 10);
+
+console.log(12 == '12');
+// sale true porque es una comparacion sencilla y lo convierte a numero
+
+// para hacerlo mas estricto le ponemos tres '='
+console.log(12 === '12');
+
+console.log('--------------------------------')
+
+let valor1 = 20;
+let valor2 = 30;
+
+//Mayor que
+console.log(valor1 > valor2);
+//Mayor o igual que
+console.log(valor1 >= valor2);
+//menor que
+console.log(valor1 < valor2);
+//menor o igual que
+console.log(valor1 <= valor2);
+//distinto que
+console.log(10 != 10);
+
+
+
