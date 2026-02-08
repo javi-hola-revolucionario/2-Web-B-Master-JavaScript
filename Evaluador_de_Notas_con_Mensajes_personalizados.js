69cb5c380fc265580d688479b7d0e1ccfac4338e
Javier Martinez <javier.martinezperches01@gmail.com>
2026-02-07 20:01:06 -0600

    feat: se crea evidencia 2 de la tarea de notas personalizadas

---
+++ Evaluador_de_Notas_con_Mensajes_personalizados.js
@@ -0,0 +1,16 @@
+// Evaluador de Notas
+
+let nota = 89
+
+if (nota >= 90) { 
+    console.log("Excelente")
+}
+else if (nota <= 89 && nota >= 75) { 
+    console.log("Bien") 
+}
+else if(nota <= 74 && nota >= 60) { 
+    console.log("Suficiente")
+}
+else if(nota < 60) { 
+    console.log("Reprobado")
+}
