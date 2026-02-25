dac2d781dbe091f0be3187043a1df0ff8e9abc61
Javier Martinez <javier.martinezperches01@gmail.com>
2026-02-24 19:22:49 -0600

    Clase de localStorage

--- main3.js
+++ main3.js
@@ -47,4 +47,55 @@ let fechaP = document.getElementById('fecha');
 let btnGuardar = document.getElementById('guardar');
 let btnEliminarr = document.getElementById('eliminar');
 
-// nos quedamos en minuto 33:20
+// Declarar las claves donde se van a guardar en localStorage
+
+const CLAVE_TEXTO = "diario_texto";
+const CLAVE_FECHA = "diario_fecha"
+
+// Funcion para obtener la fecha del dia de hoy
+
+function fechaSoloDia () {
+    let hoy = new Date(); // Creamos un objeto con la fecha actual
+    return hoy.toLocaleDateString('es-MX')
+}
+
+function cargar() {
+    let textoGuardado = localStorage.getItem(CLAVE_TEXTO)
+    let fechaGuardada = localStorage.getItem(CLAVE_FECHA)
+
+    if(textoGuardado){
+        txt.value = textoGuardado;
+    }
+
+    if (fechaGuardada){
+        fechaP.textContent = 'Guardado el: ' + fechaGuardada;
+    } else {
+        fechaGuardada.textContent = '';
+    }
+}
+
+// Para darle click en Guardar 
+btnGuardar.onclick = () => {
+    let texto = txt.value.trim(); // Para quitar espacios
+
+    if (!texto) return;
+
+    let hoy = fechaSoloDia(); // Funcion que creamos para obtener el dia
+
+    // Guardado como string
+    localStorage.setItem(CLAVE_TEXTO, texto)
+    localStorage.setItem(CLAVE_FECHA, hoy)
+
+    fechaP.textContent = 'Guardado el: ' + hoy;
+};
+
+// Para darle click en Eliminar
+btnEliminarr.onclick = () => {
+    localStorage.removeItem(CLAVE_TEXTO)
+    localStorage.removeItem(CLAVE_FECHA)
+
+    txt.value = '';
+    fechaP.textContent = '';
+};
+
+cargar(); // Llama a la funcion cargar al incio para mostrar lo que ya estaba guardado
